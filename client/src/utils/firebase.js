78bcb1adacd839fc5c747176a48185131f68cb09
Unreal-Vikhyat <vikhyatsharma137@gmail.com>
2026-04-14 16:04:22 +0530

    Project uploadation on github

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+   apiKey: "AIzaSyBXouVq2LRR3NbfvU1MjVMSvLXqRYxeiqM",
+  authDomain: "examnotesai-9f0df.firebaseapp.com",
+  projectId: "examnotesai-9f0df",
+  storageBucket: "examnotesai-9f0df.firebasestorage.app",
+  messagingSenderId: "286518424024",
+  appId: "1:286518424024:web:33bdf5d210ced37ea882ad",
+  measurementId: "G-9ZHYKD2RJF"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+
+const provider = new GoogleAuthProvider()
+
+export {auth , provider}
